a98c77cbbb8a29b87cfe9e2576dd8f05446a9572
sofkoshis <26skoshy@derryfield.org>
2024-11-07 21:51:58 -0500

    Actual Fix Lesson10PartC

--- Fix.java
+++ Fix.java
@@ -23,7 +23,7 @@
 
 import java.util.Scanner;
 
-public class App {
+public class Fix {
     public static void main(String[] args) throws Exception {
         // cookies are $0.99 apiece.
         final double COST_OF_COOKIE = 0.99;
@@ -31,23 +31,27 @@ public class App {
         // prompt user for the number of cookies they are buying.
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number of cookies: ");
-        int num = in.next;
+        int num = in.nextInt();
 
         // calculate the cost before discount.
         double cost = num * COST_OF_COOKIE;
 
         // Use conditional statements to determine what discount is.
         double discount;
-        if (num > 6) {
-            discount = 0.025;
+        if (num > 24) {
+            discount = 0.15;
         } else if (num > 12) {
             discount = 0.05;
-        } else if (num > 24) {
-            discount = 0.15;
-        }
+        } else if (num > 6) {
+           discount = 0.025;
+        } else { 
+           discount = 0.0;
 
+        }
+ 
         // calculate and display values to user.
-        cost = cost * (1 - discount);
+        cost = cost*(1-discount);
         System.out.printf("For %d cookies, your total cost is $%.2f", num, cost);
     }
 }
+
