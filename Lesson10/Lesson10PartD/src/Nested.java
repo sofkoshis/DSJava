e5ab089627e8a5e8ce50983ec559df47b920e7f8
sofkoshis <26skoshy@derryfield.org>
2024-11-07 22:26:36 -0500

    Lesson10PartD

---
+++ Lesson10/Lesson10PartD/src/Nested.java
@@ -0,0 +1,19 @@
+public class Nested {
+    public static void main(String[] args)  {
+
+    for (int  dots = 1; dots<= 5; dots++) {
+        //make the spaces before numbers
+        
+        for (int space= 5; space > dots; space--) {
+            System.out.print(".");
+        }
+            //make the 'dots' print 'dots' times ex. 1 printed one time 2 printed two times
+        for (int num = 1; num <= dots; num++) {
+            System.out.printf("%d", dots);
+        }
+        }
+
+
+    }
+    }
+
